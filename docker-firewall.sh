bcd22b81a93305b6631188affd958f71ecc36261
Fabrice TOPPI <ftoppi@xentoo.info>
2026-02-27 16:09:22 +0100

    disable shellcheck SC2018,SC2019

--- docker-firewall.sh
+++ docker-firewall.sh
@@ -136,7 +136,9 @@ process_container_policy() {
 
     echo "$1" | while read -r _chain _action; do
         _log "DEBUG" "Policy Chain=$_chain Action=$_action"
+        # shellcheck disable=SC2018,SC2019
         _chain="$(echo "$_chain" | sed -e 's/^.*\.\([A-Z]*\)":.*$/\1/' | tr 'a-z' 'A-Z')"
+        # shellcheck disable=SC2018,SC2019
         _action="$(echo "$_action" | cut -d '"' -f 2 | tr 'a-z' 'A-Z')"
 
         if [[ ! "$_chain" =~ ^(INPUT|OUTPUT|FORWARD)$ ]]; then
